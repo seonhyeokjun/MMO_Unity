9bf0f1d8d69cdaa7e5d0bf0004bf27c151bd5320
seonhyeokjun <tjsrmatn@naver.com>
2022-12-18 22:32:01 +0900

    State Machine

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -23,8 +23,6 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction += OnMouseClicked;
     }
 
-    float wait_run_ratio = 0;
-
     public enum PlayerState
     {
         Die,
@@ -34,13 +32,9 @@ public class PlayerController : MonoBehaviour
     
     PlayerState _state = PlayerState.Idle;
     
-    private void UpdateIdle()
+    private void UpdateDie()
     {
-        // 애니메이션
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
-        Animator anim = GetComponent<Animator>();
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        // 아무것도 못함
     }
 
     private void UpdateMoving()
@@ -57,18 +51,21 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
         }
         
-        // 애니메이션 처리
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
+        // 애니메이션
+        // wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
         Animator anim = GetComponent<Animator>();
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
-    }
-
-    private void UpdateDie()
-    {
-        // 아무것도 못함
+        // 현재 게임 상태에 대한 정보를 넘겨준다
+        anim.SetFloat("speed", _speed);
     }
     
+    private void UpdateIdle()
+    {
+        // 애니메이션
+        // wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("speed", 0);
+    }
+
     void Update()
     {
         // if (_moveToDest)
