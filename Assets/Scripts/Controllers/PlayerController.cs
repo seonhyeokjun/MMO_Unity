7e976056149994c2031a2b5d38d4146086037e89
seonhyeokjun <tjsrmatn@naver.com>
2023-05-03 02:19:10 +0900

    Camera

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -12,15 +12,33 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
     
+    bool _moveToDest = false;
+    Vector3 _destPos;
+    
     void Start()
     {
         Managers.Input.KeyAction -= Onkeyboard;
         Managers.Input.KeyAction += Onkeyboard;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
     void Update()
     {
-        
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.0001f)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                transform.position += dir.normalized * moveDist;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+            }
+        }
     }
 
     void Onkeyboard()
@@ -51,6 +69,26 @@ public class PlayerController : MonoBehaviour
             // transform.rotation = Quaternion.LookRotation(Vector3.right);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             transform.position += Vector3.right * (Time.deltaTime * _speed);
+        }
+        
+        _moveToDest = false;
+    }
+    
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click)
+        {
+            return;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _moveToDest = true;
         }
     }
 }
