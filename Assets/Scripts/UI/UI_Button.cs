c39bad1de9d58a0669697f1104506087df264937
seonhyeokjun <tjsrmatn@naver.com>
2023-05-12 00:03:56 +0900

    UI 자동화1

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 public class UI_Button : MonoBehaviour
 {
-    [SerializeField] 
-    Text _text;
+    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+    enum Buttons
+    {
+        PointButton,
+    }
+    
+    enum Texts
+    {
+        PointText,
+        ScoreText,
+    }
+    
+    void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+    }
+
+    void Bind<T>(Type type) where T : Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+        }
+    }
 
     private int _score = 0;
     
     public void OnButtonClicked()
     {
         _score++;
-        _text.text = $"점수 : {_score}";
     }
 }
